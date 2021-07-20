45acb2e76700080c0f7de3c7df6cd8b1ef6b2ca0
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2021-07-20 02:25:15 -0700

    Upgrade to Spring-boot 2.5.2

--- src/test/java/com/oreilly/demo/SpringIntegrationDemoApplicationTests.java
+++ src/test/java/com/oreilly/demo/SpringIntegrationDemoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.oreilly.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringIntegrationDemoApplication.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class SpringIntegrationDemoApplicationTests {
 
 	@Test
