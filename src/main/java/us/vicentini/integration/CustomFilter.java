9ec039a9560e6549632d086da46313481c3b4108
Henrique Vicentini <henrique@vicentini.us>
2021-08-15 18:18:26 -0700

    S05L37. Spring Integration Component: Learn About Custom Filters

---
+++ src/main/java/us/vicentini/integration/CustomFilter.java
@@ -0,0 +1,11 @@
+package us.vicentini.integration;
+
+import org.springframework.integration.core.MessageSelector;
+import org.springframework.messaging.Message;
+
+public class CustomFilter implements MessageSelector {
+    @Override
+    public boolean accept(Message<?> message) {
+        return message.getPayload().equals(7);
+    }
+}
---
+++ src/main/java/us/vicentini/integration/runner/endpoint/CustomFilterAppRunner.java
@@ -0,0 +1,54 @@
+package us.vicentini.integration.runner.endpoint;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import us.vicentini.integration.PrinterGateway;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(value = "runner.endpoint.CustomFilterAppRunner", havingValue = "true",
+        matchIfMissing = true)
+public class CustomFilterAppRunner implements ApplicationRunner {
+
+    private final PrinterGateway customFilterGateway;
+
+
+    @SneakyThrows
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("RUNNING CustomFilterAppRunner !!!");
+        List<Future<Message<String>>> futures = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Message<?> message = getMessage(i);
+
+            log.info("Sending Message: {}", message);
+            futures.add(customFilterGateway.print(message));
+        }
+        log.info("Reading responses");
+        for (Future<Message<String>> future : futures) {
+            log.info("Future message: {}", future.get().getPayload());
+        }
+        log.info("------------------------------------");
+    }
+
+
+    private Message<?> getMessage(int i) {
+        MessageBuilder<?> returnValue;
+            returnValue = MessageBuilder.withPayload(i)
+                    .setHeader("routeHeader", "int");
+        return returnValue
+                .setHeader("messageNumber", i).build();
+    }
+}
